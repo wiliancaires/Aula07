62862235f0062e2d5bf84469c44b50ebeaf8c932
edson <engsoftware1@gmail.com>
2016-09-15 13:52:07 -0300

    versao inicial

---
+++ sceweb/src/br/sceweb/controle/ServletControle.java
@@ -0,0 +1,19 @@
+package br.sceweb.controle;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import br.sceweb.modelo.Empresa;
+
+
+/**
+ * Servlet implementation class ServletControle
+ */
+public class ServletControle extends HttpServlet {
+	
+}
---
+++ sceweb/src/br/sceweb/servico/FabricaDeConexoes.java
@@ -0,0 +1,12 @@
+package br.sceweb.servico;
+
+import java.sql.DriverManager;
+
+import org.apache.log4j.Logger;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
+
+public class FabricaDeConexoes {
+	
+}
